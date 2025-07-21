6e05d24c592b4153b64388ed3c4ff58a7a1f714f
JoyKalombo <112102600+JoyKalombo@users.noreply.github.com>
2025-07-21 19:08:45 +0100

    emotion document removed and emotion questions appended to open ended questions document

--- pages/Play.py
+++ pages/Play.py
@@ -97,13 +97,11 @@ def load_question_bank():
     open_qs = load_json("questions-open_ended.json")
     mc_qs = load_json("questions-multiple_choice.json")
     pick_qs = load_json("questions-pick_a_player.json")
-    emotion_qs = load_json("questions-emotions.json")
 
     return (
         [{"type": "open", "question": q} for q in open_qs] +
         [{"type": "mc", **q} for q in mc_qs] +
-        [{"type": "pick", "question": q} for q in pick_qs] +
-        [{"type": "emotion", "question": q} for q in emotion_qs]
+        [{"type": "pick", "question": q} for q in pick_qs]
     )
 
 
@@ -153,8 +151,6 @@ if room_id and player_name:
             elif question_data.get("type") == "pick":
                 current_players = list(get_player_list(room_id).keys())
                 player_answer = st.radio("Pick a player among us:", current_players, key="pick")
-            elif question_data.get("type") == "emotion":
-                player_answer = st.text_input("Which emotion do you think it is?")
             else:
                 player_answer = st.text_input("Your Answer")
         else:
