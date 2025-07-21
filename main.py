59bb0f18dfee3a6ba4c233d3ff39b88c40cd6e8c
JoyKalombo <112102600+JoyKalombo@users.noreply.github.com>
2025-07-21 17:31:42 +0100

    game logic on separate page and main page has buttons to summon sheep

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ st.title("🐏 Sheepish Mentality 🐑🧠🤣🌾 - Multiplayer Game")
 st.subheader("Play together in real time with your friends!")
 
 if st.button("Summon Sheep"):
-    st.image("https://media.giphy.com/media/13borq7Zo2kulO/giphy.gif", caption="A wild sheep appears!")
+    st.image("https://media.giphy.com/media/3ohhwehR8oQSD0W9Fu/giphy.gif", caption="A wild sheep appears!")
 
 if st.button("Sheepify the Stream"):
     st.markdown("🐑🐑🐑 Baa baa black sheep... 🐑🐑🐑")
