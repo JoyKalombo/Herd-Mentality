612fa0c2603bb6d09c65f0bdac756fbdc2bec51e
JoyKalombo <112102600+JoyKalombo@users.noreply.github.com>
2025-07-21 02:39:56 +0100

    trying again with the streamlit secrents

--- pages/Play.py
+++ pages/Play.py
@@ -13,7 +13,8 @@ from streamlit_autorefresh import st_autorefresh
 if not firebase_admin._apps:
     cred = credentials.Certificate(json.loads(st.secrets["firebase_creds"]))
     firebase_admin.initialize_app(cred, {
-        'databaseURL': os.getenv("FIREBASE_DB_URL")
+        'databaseURL': st.secrets["firebase_db_url"]
+
     })
 
 # --- Firebase Helpers ---
